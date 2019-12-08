a5e6748911c6010962d1d82b4b699e8db2cfcee7
raven724 <kangwogus3@gmail.com>
2019-12-08 23:55:58 +0900

    check and fix bugs

--- src/Messenger/views.py
+++ src/Messenger/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 
 
@@ -8,18 +8,33 @@ def ok_to_load_in_a_frame(request):
 
 
 def messenger(request):
-    return render(request, 'Messenger/messenger.html')
+    try:
+        username = request.session['uname']
+        content = {
+            'username': username,
+            'login': 'yes',
+        }
+        return render(request, 'Messenger/messenger.html', context=content)
+    except:
+        return HttpResponseRedirect('/user/login/')
 
 
 def list_messenger(request):
+    '''
     context = {
         'username': 'user2'
     }
     return render(request, "Messenger/chatlist.html", context=context)
-
+    '''
+    username = request.session['uname']
+    content = {
+        'username': username,
+    }
+    return render(request, 'Messenger/chatlist.html', context=content)
 
 def detail_messenger(request):
-    context = {
-        'username': 'user2'
+    username = request.session['uname']
+    content = {
+        'username': username,
     }
-    return render(request, "Messenger/chatform.html", context=context)
+    return render(request, "Messenger/chatform.html", context=content)
